654d5fc24fa3ade4958e262ee3e8c40588efbf49
Luuk Knippenberg <luuk_knippenberg@hotmail.com>
2022-08-07 20:19:48 +0200

    Created 728x90

---
+++ beach-resort-makkum/appartementen/728x90/script.js
@@ -0,0 +1,59 @@
+window.addEventListener('load', function () {
+    
+    var currentStage = 1;
+    var amountOfStages = 6;
+
+    // Skips the first slide
+    var timePerSlide =  [0, 750, 1500, 1500, 1500, 2000, 2500]
+    var element = document.querySelector('#banner');
+
+    var next = document.querySelector('#next');
+    var stage = document.querySelector('#currentStage');
+    var prev = document.querySelector('#prev');
+
+    var stageTimer = function() {
+        moveToNextStage()
+        setTimeout(stageTimer, timePerSlide[(currentStage)]);
+    }
+    if(!next){
+        setTimeout(stageTimer, timePerSlide[currentStage]);
+    }
+    
+
+    function moveToNextStage(){
+
+        element.classList.remove(("stage-" + currentStage))
+
+        if(currentStage < amountOfStages)
+        {
+            currentStage++;
+        }
+        else{
+            currentStage = 1; 
+        }
+        
+        var stageToAdd = "stage-" + currentStage;
+
+        element.classList.add(stageToAdd)
+
+        if(stage){
+            stage.innerHTML = currentStage;
+        }
+        
+    }
+    if(next){
+        next.onclick = function(){
+            moveToNextStage();
+        }
+    }
+    
+})
+
+var clickArea = document.getElementById('clickTag1');
+
+var clickTAGvalue = dhtml.getVar('clickTAG', '#');
+var landingpagetarget = dhtml.getVar('landingPageTarget', '_blank');
+
+clickArea.onclick = function() {
+    window.open(clickTAGvalue,landingpagetarget);
+}   
