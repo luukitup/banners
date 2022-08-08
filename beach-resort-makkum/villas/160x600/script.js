6e930687db54879a6389572b11d67ef5556c7724
Luuk Knippenberg <luuk_knippenberg@hotmail.com>
2022-08-08 18:13:49 +0200

    Updated clickTags

--- beach-resort-makkum/villas/160x600/script.js
+++ beach-resort-makkum/villas/160x600/script.js
@@ -51,7 +51,7 @@ window.addEventListener('load', function () {
 
 var clickArea = document.getElementById('clickTag1');
 
-var clickTAGvalue = dhtml.getVar('clickTAG', 'https://www.residencemakkum.nl/?utm_medium=display&utm_source=banner&utm_campaign=beach-resort-makkum&utm_content=appartement');
+var clickTAGvalue = dhtml.getVar('clickTAG', 'https://www.residencemakkum.nl/?utm_medium=display&utm_source=banner&utm_campaign=beach-resort-makkum&utm_content=villa');
 var landingpagetarget = dhtml.getVar('landingPageTarget', '_blank');
 
 clickArea.onclick = function() {
