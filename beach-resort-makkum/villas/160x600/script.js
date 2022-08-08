8a729f9abe0e1fd2457168d174cb8edfe5627da1
Luuk Knippenberg <luuk_knippenberg@hotmail.com>
2022-08-08 18:01:38 +0200

    Fixed 160x600

--- beach-resort-makkum/villas/160x600/script.js
+++ beach-resort-makkum/villas/160x600/script.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', function () {
     
     var currentStage = 1;
-    var amountOfStages = 6; 
+    var amountOfStages = 5; 
 
     // Skips the first slide
-    var timePerSlide =  [0, 500, 2500, 1500, 2500, 3000, 3000]
+    var timePerSlide =  [0, 500, 2500, 1500, 3000, 3000]
     var element = document.querySelector('#banner');
 
     var next = document.querySelector('#next');
