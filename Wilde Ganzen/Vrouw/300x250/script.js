79fb09ab99574b38512f5beeb6c5532ea3a427b1
Luuk Knippenberg <luuk_knippenberg@hotmail.com>
2022-12-14 17:43:34 +0100

    Wilde Ganzen Vrouwen - Created 300x250

---
+++ Wilde Ganzen/Vrouw/300x250/script.js
@@ -0,0 +1,59 @@
+window.addEventListener('load', function () {
+    
+    var currentStage = 1;
+    var amountOfStages = 4; // 6 Stages
+
+    // Skips the first slide
+    var timePerSlide =  [0, 2000, 2000, 1500, 3500]
+    var element = document.querySelector('#banner');
+
+    var next = document.querySelector('#next');
+    var stage = document.querySelector('#currentStage');
+    var prev = document.querySelector('#prev');
+
+    var stageTimer = function() {
+        moveToNextStage()
+        setTimeout(stageTimer, timePerSlide[(currentStage)]);
+    }
+    if(!next){
+        setTimeout(stageTimer, timePerSlide[currentStage]);
+    }
+    
+
+    function moveToNextStage(){
+
+        element.classList.remove(("stage-" + currentStage))
+
+        if(currentStage < amountOfStages)
+        {
+            currentStage++;
+        }
+        else{
+            currentStage = 1; 
+        }
+        
+        var stageToAdd = "stage-" + currentStage;
+
+        element.classList.add(stageToAdd)
+
+        if(stage){
+            stage.innerHTML = currentStage;
+        }
+        
+    }
+    if(next){
+        next.onclick = function(){
+            moveToNextStage();
+        }
+    }
+    
+})
+
+var clickArea = document.getElementById('clickTag1');
+
+var clickTAGvalue = dhtml.getVar('clickTAG', 'https://www.wildeganzen.nl/');
+var landingpagetarget = dhtml.getVar('landingPageTarget', '_blank');
+
+clickArea.onclick = function() {
+    window.open(clickTAGvalue,landingpagetarget);
+}   
