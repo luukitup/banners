b315ae3f19d192573c0eb9cf43c61f4a7d7c48c8
Luuk <luuk@luukitup.nl>
2023-03-23 14:55:05 +0100

    Implemented 320x240, 320x250

---
+++ keukenhof/320x240_keukenhof/script.js
@@ -0,0 +1,58 @@
+window.addEventListener('load', function () {
+    
+    var currentStage = 1;
+
+    // Skips the first slide
+    var timePerSlide =  [2500, 2500, 2500, 100]
+    var element = document.querySelector('#banner');
+
+    var next = document.querySelector('#next');
+    var stage = document.querySelector('#currentStage');
+    var prev = document.querySelector('#prev');
+
+    var stageTimer = function() {
+        moveToNextStage()
+        setTimeout(stageTimer, timePerSlide[currentStage - 1]);
+    }
+    if(!next){
+        setTimeout(stageTimer, timePerSlide[currentStage - 1]);
+    }
+    
+
+    function moveToNextStage(){
+
+        element.classList.remove(("stage-" + currentStage))
+
+        if(currentStage < timePerSlide.length)
+        {
+            currentStage++;
+        }
+        else{
+            currentStage = 1; 
+        }
+        
+        var stageToAdd = "stage-" + currentStage;
+
+        element.classList.add(stageToAdd)
+
+        if(stage){
+            stage.innerHTML = currentStage;
+        }
+        
+    }
+    if(next){
+        next.onclick = function(){
+            moveToNextStage();
+        }
+    }
+    
+})
+
+var clickArea = document.getElementById('clickTag1');
+
+var clickTAGvalue = dhtml.getVar('clickTAG', 'https://keukenhof.nl/nl/');
+var landingpagetarget = dhtml.getVar('landingPageTarget', '_blank');
+
+clickArea.onclick = function() {
+    window.open(clickTAGvalue,landingpagetarget);
+}   
